34e96d20aa56c4b5ab6c9b73e93d08f3465e63b1
Joshua Choi <121473687+distant-past@users.noreply.github.com>
2023-03-08 02:30:22 -0800

    create a button that allows you to input a number and reset the grid to have that number as its dimensions.

--- index.js
+++ index.js
@@ -14,8 +14,39 @@ makeRows(16, 16);
 
 const newColor = document.querySelectorAll(".grid-item");
 
-for (const color of newColor) {
-    color.addEventListener("mouseover", () => {
-        color.style.backgroundColor = "green";
-    });
+function createColor() {
+    for (const color of newColor) {
+        color.addEventListener("mouseover", () => {
+            color.style.backgroundColor = "green";
+        });
+    }
+}
+createColor();
+
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener('click', () =>{
+    inputRows();
+})
+
+function inputRows() {
+    let num = prompt('Input number for new grid:');
+    if (num > 100) {
+        alert('100 is the max number allowed!');
+        return
+    } else if (num <= 0) {
+        alert('Please enter a valid number.');
+        return
+    } else if (num <= 100 || num > 0) {
+        clearGrid();
+        makeRows(num,num);
+        createColor();
+    } else {
+        alert('Please enter a valid number.');
+    }
+}
+
+function clearGrid() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
