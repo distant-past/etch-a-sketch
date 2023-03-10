9396b46813abb6b095219ee032aa92fbd2042389
Joshua Choi <121473687+distant-past@users.noreply.github.com>
2023-03-09 18:10:24 -0800

    adjust the creating grid function to replace the child divs with new ones instead of restting them and make sure that they also change color upon hovering. also adjust css to have grid fit inside certain dimensions, even if number of grid changes.

--- index.js
+++ index.js
@@ -3,25 +3,20 @@ const container = document.getElementById("container");
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
+  container.replaceChildren();
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   };
+  const newColor = document.querySelectorAll(".grid-item");
+  for (const color of newColor) {
+    color.addEventListener("mouseover", () => {
+        color.style.backgroundColor = "green";
+    });
+}
 };
 
 makeRows(16, 16);
-
-const newColor = document.querySelectorAll(".grid-item");
-
-function createColor() {
-    for (const color of newColor) {
-        color.addEventListener("mouseover", () => {
-            color.style.backgroundColor = "green";
-        });
-    }
-}
-createColor();
 
 const resetButton = document.getElementById("reset");
 resetButton.addEventListener('click', () =>{
@@ -37,16 +32,8 @@ function inputRows() {
         alert('Please enter a valid number.');
         return
     } else if (num <= 100 || num > 0) {
-        clearGrid();
         makeRows(num,num);
-        createColor();
     } else {
         alert('Please enter a valid number.');
-    }
-}
-
-function clearGrid() {
-    while(container.firstChild) {
-        container.removeChild(container.firstChild);
     }
 }
